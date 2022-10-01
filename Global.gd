174cbe137c04cfe95390454263eace47ee5d00ff
heckin-doggo <heckindoggocrap@gmail.com>
2022-09-30 20:02:27 -0400

    Initial Commit for Ludum Dare 51 Project

---
+++ Global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Global variables defined here.
+var player_health = 15
+var player_max_health = 15
+
+var boss_name = "BOSS NAME"
+var boss_health = 10
+var boss_max_health = 10
+
