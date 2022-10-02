d823baa326e21678f378018bbaafaa3113325ad0
heckin-doggo <heckindoggocrap@gmail.com>
2022-10-02 16:05:10 -0400

    Interview Setup + JSON

--- Global.gd
+++ Global.gd
@@ -1,3 +1,18 @@
 extends Node
 
 # Global variables defined here.
+var evidence = {
+	"ReporterTestimony": false,
+	"MayorTimeline": false,
+	"EventPhoto": false,
+	"SuitcaseDiscrepency": false,
+	"Bullet": false,
+	"CallLog": false,
+	"ProtesterTestimony": false,
+	"PodiumChange": false,
+	"AssassinPlot": false,
+	"SniperTestimony": false,
+	"CarCompartment": false,
+	"ReporterPhoto": false,
+	"RefreshmentTable": false,
+}
