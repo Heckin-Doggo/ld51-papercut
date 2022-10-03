a3a6cd67c1960354dc8029936845bb3847b9a576
carsonburley <68079481+carsonburley@users.noreply.github.com>
2022-10-02 20:59:24 -0400

    level assets

---
+++ scenes/Portrait.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+signal start_interrogation
+
+export var character = ""
+
+func _ready():
+	connect("body_entered", self, "start_interrogation")
+
+func start_interrogation(body):
+	if body.get_name == "Player":
+		emit_signal("start_interrogation", character)
