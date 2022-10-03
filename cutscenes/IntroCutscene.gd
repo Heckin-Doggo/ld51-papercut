482edead9e7881bd8f02a15fd68311e1e92f3e83
Heckin-Doggo <heckindoggocrap@gmail.com>
2022-10-03 12:06:37 -0400

    Cutscene intro start work

--- cutscenes/IntroCutscene.gd
+++ cutscenes/IntroCutscene.gd
@@ -1,10 +1,13 @@
-extends Node2D
+extends Control
 
 # music singleton
 onready var Music = get_node("/root/Music")
 
 # preload the thing we want instances of
 var dialogBox = preload("res://scenes/DialogBox.tscn")
+
+# get child resources
+onready var Slide = get_node("Slide")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,27 +18,33 @@ func _ready():
 #func _process(delta):
 #	pass
 
+#func play_cutscene():
+#	# TODO: Make skipable instead of timer.
+#	yield(get_tree().create_timer(1), "timeout")
+#	$Detective.visible = true
+#	yield(get_tree().create_timer(0.5), "timeout")
+#
+#	var dialog = dialogBox.instance()
+#	dialog.dialogPath = "res://dialog/introCutscene1.json"
+#	$CanvasLayer.add_child(dialog)
+#
+#	yield(dialog, "finished")
+#
+#	yield(get_tree().create_timer(0.5), "timeout")
+#	$Paper.visible = true
+#	yield(get_tree().create_timer(0.5), "timeout")
+#
+#	dialog = dialogBox.instance()
+#	dialog.dialogPath = "res://dialog/introCutscene2.json"
+#	$CanvasLayer.add_child(dialog)
+#
+#	yield(dialog, "finished")
+#
+#	$Detective.visible = false
+
 func play_cutscene():
-	# TODO: Make skipable instead of timer.
-	yield(get_tree().create_timer(1), "timeout")
-	$Detective.visible = true
-	yield(get_tree().create_timer(0.5), "timeout")
-	
-	var dialog = dialogBox.instance()
-	dialog.dialogPath = "res://dialog/introCutscene1.json"
-	$CanvasLayer.add_child(dialog)
-	
-	yield(dialog, "finished")
-	
-	yield(get_tree().create_timer(0.5), "timeout")
-	$Paper.visible = true
-	yield(get_tree().create_timer(0.5), "timeout")
-	
-	dialog = dialogBox.instance()
-	dialog.dialogPath = "res://dialog/introCutscene2.json"
-	$CanvasLayer.add_child(dialog)
-	
-	yield(dialog, "finished")
-	
-	$Detective.visible = false
+#	Slide.texture = load("res://assets/art/cutscenes/Start1.jpg")
+	pass
+	# TODO: Fix this shit
+
 
--- menu/MainMenu.gd
+++ menu/MainMenu.gd
@@ -13,4 +13,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://scenes/TestDialog.tscn")
+		get_tree().change_scene("res://cutscenes/IntroCutscene.tscn")
